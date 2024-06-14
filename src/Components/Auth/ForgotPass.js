32d9489a63d5412e22095b2035185a6ca0636525
talhanonari <asimriaz1998@gmail.com>
2024-06-14 19:02:48 +0500

    Login & Forgot Password page Add & routes applied

---
+++ src/Components/Auth/ForgotPass.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { TextField, Button, Container, Typography, Grid, InputAdornment, Link, FormControl, Box, createTheme, ThemeProvider } from '@mui/material';
+import EmailIcon from '@mui/icons-material/Email';
+import { useNavigate } from 'react-router-dom';
+
+const theme = createTheme({
+  palette: {
+    grayish: {
+      main: '#f4f4f4',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+});
+
+const ForgotPass = () => {
+  const navigate = useNavigate();
+    const handleSubmit = (event) => {
+    event.preventDefault();
+};
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="100vw" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+        <div>
+        <Box style={{ width: '100%', maxWidth: '400px', textAlign: 'center' }}>
+        <Typography component="h1" variant="h3" gutterBottom style={{ fontWeight: 'bold', margin:'1.5rem 0'}}>
+           locaidad
+          </Typography>
+          <Typography component="h1" variant="h5" gutterBottom style={{ fontWeight: 'bold', margin:'1.5rem 0' }}>
+            Reset Your Password
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <TextField
+                    fullWidth
+                    variant="outlined"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    placeholder="Email Address"
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <EmailIcon />
+                        </InputAdornment>
+                      ),
+                      sx: {
+                        backgroundColor: theme.palette.grayish.main,
+                        '&:focus': {
+                          borderColor: theme.palette.primary.main,
+                        },
+                      },
+                    }}
+                    sx={{
+                      '& .MuiOutlinedInput-root': {
+                        borderRadius: '10px',
+                        height: '3rem',
+                      },
+                      '& .MuiInputBase-input': {
+                        padding: '10px 14px',
+                      },
+                    }}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    marginTop: '0.5rem',
+                    borderRadius: '50px',
+                    backgroundColor: '#ff2557',
+                    color: 'white',
+                    height: '2.75rem',
+                  }}
+                >
+                  Forgotten Password
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body2" color="textSecondary">
+                Want to Login?{' '}
+                  <span style={{ cursor: 'pointer', textDecoration: 'underline', color: '#ff2557' }} onClick={() => navigate('/login')}>
+                    Login
+                  </span>
+                </Typography>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+        <div className='container' style={{ marginTop: '2rem' }}>
+            <Grid container spacing={2} justifyContent="center">
+            <Grid item>
+                <Link href="#" color="inherit">
+                  <Typography variant="body2">About the App</Typography>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" color="inherit">
+                  <Typography variant="body2">Terms and Conditions</Typography>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" color="inherit">
+                  <Typography variant="body2">Privacy Policy</Typography>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" color="inherit">
+                  <Typography variant="body2">Imprint</Typography>
+                </Link>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default ForgotPass;
---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './Components/Home/Home'
+import Login from './Components/Auth/Login'
+import ForgotPass from './Components/Auth/ForgotPass'
+import Signup from './Components/Auth/Signup'
+
+const Router = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Home days={120}/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/forgot' element={<ForgotPass/>}/>
+    </Routes>
+  )
+}
+
+export default Router;

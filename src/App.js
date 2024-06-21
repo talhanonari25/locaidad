4b3605a183c89f23012aab83a0ddfb8935bd40fc
talhanonari <asimriaz1998@gmail.com>
2024-06-21 15:52:24 +0500

    dark & light Theme Apply

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react';
 import Router from './Routes'
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { IconButton } from '@mui/material';
+import { DarkMode, LightMode } from '@mui/icons-material';
 
 const App = () => {
-  return (
-    <div>
-      <Router/>
-    </div>
-  )
-}
+  const [darkMode, setDarkMode] = useState(false);
 
-export default App
+  const lightTheme = useMemo(() => createTheme({
+    palette: {
+      mode: 'light',
+    },
+  }), []);
+
+  const darkTheme = useMemo(() => createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  }), []);
+
+  const theme = darkMode ? darkTheme : lightTheme;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+          <IconButton
+            style={{textAlign:'left'}}
+            color="inherit"
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            {darkMode ? <LightMode /> : <DarkMode />}
+          </IconButton>
+      <Router/>
+    </ThemeProvider>
+  );
+};
+
+export default App;
